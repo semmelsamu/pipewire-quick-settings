29550605b96e428cd6b336b74f67b5cfc51c330c
Samuel Kroiß <mail@samuelkroiss.de>
2025-07-14 18:09:12 +0200

    Add initial gtk boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button};
+
+fn main() -> glib::ExitCode {
+    let application = Application::builder()
+        .application_id("com.github.semmelsamu.pipewire-quick-settings")
+        .build();
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("Pipewire Quick Settings")
+            .default_width(350)
+            .default_height(70)
+            .build();
+
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+        window.set_child(Some(&button));
+
+        window.present();
+    });
+
+    application.run()
 }
