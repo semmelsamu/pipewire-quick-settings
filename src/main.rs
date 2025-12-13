bf274e3b9a3dcb4d4475ffb975c29e1eb87ca237
Samuel Kroiß <mail@samuelkroiss.de>
2025-12-13 19:32:42 +0100

    Add serde json parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::process::Command;
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("pw-dump")
+        .output()
+        .expect("Error running pw-dump");
+
+    if !output.status.success() {
+        panic!("{}", String::from_utf8_lossy(&output.stderr));
+    }
+
+    let data: Value = serde_json::from_slice(&output.stdout).unwrap();
+    println!("{}", data[0]);
 }
