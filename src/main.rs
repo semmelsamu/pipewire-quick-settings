705461fc0423c5057bc4c3db4fd64c3ef6175dfa
Samuel Kroiß <mail@samuelkroiss.de>
2025-12-14 20:57:25 +0100

    Add default sink logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::process::Command;
 use serde_json::Value;
+use std::io;
+use std::io::Write;
 
 fn main() {
     let output = Command::new("pw-dump")
@@ -11,5 +13,53 @@ fn main() {
     }
 
     let data: Value = serde_json::from_slice(&output.stdout).unwrap();
-    println!("{}", data[0]);
+    
+    let sinks = audio_sinks(&data);
+    
+    for s in &sinks {
+        println!("{}", serde_json::to_string(&s).unwrap());
+    }
+    
+    for s in &sinks {
+        println!("{} {}", s["id"], s["info"]["props"]["node.description"]);
+    }
+    
+    print!("Choose sink: > ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+        
+    print!("Chose sink {}", input);
+    
+    wpctl_set_default(input);
+}
+
+fn audio_sinks(data: &Value) -> Vec<&Value> {
+    data.as_array()
+        .into_iter()
+        .flatten()
+        .filter(|obj| {
+            obj.get("type")
+                .and_then(Value::as_str)
+                == Some("PipeWire:Interface:Node")
+            &&
+            obj.get("info")
+                .and_then(|i| i.get("props"))
+                .and_then(|p| p.get("media.class"))
+                .and_then(Value::as_str)
+                == Some("Audio/Sink")
+        })
+        .collect()
+}
+
+fn wpctl_set_default(sink_id: String) {
+    let output = Command::new("wpctl")
+        .arg("set-default")
+        .arg(sink_id)
+        .output()
+        .expect("Error setting default sink");
 }
