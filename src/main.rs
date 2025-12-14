3ea8c32524c5736258306a119d0397f4e8a7e0cf
Samuel Kroiß <mail@samuelkroiss.de>
2025-12-14 22:22:50 +0100

    Add basic cli loop

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,47 @@ mod pipewire;
 mod parsers;
 
 use utils::prompt;
-use serde_json::Value;
 use pipewire::{pw_dump, wpctl_set_default};
 use parsers::{default_sink, audio_sinks};
 
 fn main() {
-    let input = prompt("What do you want to do?");
+    println!("PipeWire Quick Settings");
     
-    match input {
-        _ => panic!("No valid option")
-    }
-    
-    let data = pw_dump();
-  
-    let sinks = audio_sinks(&data);
-    
-    println!("Default sink name: {:?}", default_sink(&data));
-    
-    for s in &sinks {
-        println!("{} {}", s["id"], s["info"]["props"]["node.description"]);
-    }
-    
-    let input = prompt("Choose sink");
-    let sink_id: u32 = input.trim().parse().expect("Invalid sink id");
+    loop {
+        println!();
+        println!("0 = Exit");
+        println!("1 = Show default sink");
+        println!("2 = Set default sink");
+        
+        let input = prompt("What do you want to do?");
 
-    println!("Chose sink {}", sink_id);
- 
-    wpctl_set_default(sink_id);
+        match input.trim() {
+            "0" => break,
+            "1" => {
+                let data = pw_dump();
+                match default_sink(&data) {
+                    Some(name) => println!("Default sink: {}", name),
+                    None => println!("Default sink not found"),
+                }
+            }
+            "2" => {
+                let data = pw_dump();
+                let sinks = audio_sinks(&data);
+
+                for s in &sinks {
+                    println!("{} {}", s["id"], s["info"]["props"]["node.description"]);
+                }
+
+                let input = prompt("Choose sink id");
+                match input.trim().parse::<u32>() {
+                    Ok(sink_id) => {
+                        wpctl_set_default(sink_id);
+                        println!("Set default sink to {}", sink_id);
+                    }
+                    Err(_) => println!("Invalid sink id"),
+                }
+            }
+            _ => println!("Invalid option"),
+        }
+    }
 }
