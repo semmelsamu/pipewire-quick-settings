dc3f65d9136b1ae8eb9bfa9854964cf5f7b35bd5
Samuel Kroiß <mail@samuelkroiss.de>
2025-12-15 14:13:55 +0100

    Move u32 function to utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use serde_json::Value;
 use std::io::{self, Write};
 
 pub fn prompt(question: &str) -> String {
@@ -11,4 +12,12 @@ pub fn prompt(question: &str) -> String {
         .expect("Failed to read line");
         
     input
+}
+
+pub fn value_as_u32(value: &Value) -> Option<u32> {
+    match value {
+        Value::Number(n) => n.as_u64().and_then(|n| u32::try_from(n).ok()),
+        Value::String(s) => s.parse::<u32>().ok(),
+        _ => None,
+    }
 }
